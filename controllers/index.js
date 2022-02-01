1b5d3d062d0100f15d3f3bbbdd73931b554842d8
poojapatel815 <ppate815@my.centennialcollege.ca>
2022-02-01 15:02:04 +0530

    Update index.js
    Add about, projects, services and contact me pages

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,17 @@ exports.home = function(req, res, next) {
 }
 
 exports.projects = function(req, res, next) {
-    res.render('index', { title: 'Projects' });
+    res.render('projects', { title: 'Projects' });
 }
 
 exports.about = function(req, res, next) {
-    res.render('index', { title: 'About' });
+    res.render('about', { title: 'About' });
+}
+
+exports.services = function(req, res, next) {
+    res.render('services', { title: 'services' });
+}
+
+exports.contact = function(req, res, next) {
+    res.render('contact', { title: 'contact me' });
 }
