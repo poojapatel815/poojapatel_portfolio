dc3b40e7cbc2752229b93993d95f15d3113561b9
poojapatel815 <ppate815@my.centennialcollege.ca>
2022-02-22 13:34:17 +0530

    Update app.js

--- config/app.js
+++ config/app.js
@@ -3,6 +3,10 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let session = require('express-session');
+let flash = require('connect-flash');
+let passport = require('passport');
+
 
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
@@ -10,6 +14,12 @@ let inventoryRouter = require('../routes/inventory');
 
 
 let app = express();
+
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: "sessionSecret"
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
@@ -21,6 +31,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
+
+// Sets up passport
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
