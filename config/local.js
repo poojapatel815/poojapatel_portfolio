fc70eea80c8dfa4f4a0065cc7e6812cc3a16ccdc
poojapatel815 <ppate815@my.centennialcollege.ca>
2022-02-22 13:34:20 +0530

    Update local.js

--- config/local.js
+++ config/local.js
@@ -1,6 +1,6 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
 
 module.exports = function() {
     console.log('====> LocalStrategy function')
